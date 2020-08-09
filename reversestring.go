f86b4efd1dc82156b7aa58326ae58b41cf72e579
Shawn Wollenberg <shawnwollenberg@users.noreply.github.com>
2020-08-09 11:14:17 -0400

    quick cleanup

--- reversestring.go
+++ reversestring.go
@@ -1,7 +1,6 @@
 package Reversestring
 
 func Reversestring(str string) (result string) {
-	//fmt.Println(str)
 	for _, v := range str {
 		result = string(v) + result
 	}
@@ -11,20 +10,7 @@ func Reversestring(str string) (result string) {
 func Reversstring_backup(s string) string {
 	rns := []rune(s) // convert to rune
 	for i, j := 0, len(rns)-1; i < j; i, j = i+1, j-1 {
-
-		// swap the letters of the string,
-		// like first with last and so on.
 		rns[i], rns[j] = rns[j], rns[i]
 	}
-
-	// return the reversed string.
 	return string(rns)
 }
-
-/*func main() {
-	fmt.Println(Reversestring("shawn"))
-}*/
-
-//create _test.go
-//go mod init
-//run "go test"
